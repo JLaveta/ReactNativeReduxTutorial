f31c60c1ed9ac07f52d0c2e1e6b3be89352d4c14
John Laveta <lavetajm@gmail.com>
2018-12-03 17:49:30 -0800

    Fetching working. State setting with response data.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+    //Setup a default state with an object that contains an empty list of albums
+    state = { albums: [] };
+
+    //Fetches the data during the componentWillMount lifecycle function.
+    //Take the response data and set the state with this data
     componentWillMount() {
-        console.log("componentWillMount in AlbumList");
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
     }
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album list!!!</Text>
